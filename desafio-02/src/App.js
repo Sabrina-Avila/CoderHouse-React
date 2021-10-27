132853910659c285960b9f3f1700d90fb2afd77f
Sabrina Avila <sabrina.sol.avila@gmail.com>
2021-10-27 18:32:20 +0200

    Agrego desafio 2

---
+++ desafio-02/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+    <div>
+      
+        
+        <NavBar/>
+         
+     
+    </div>
+  );
+}
+
+export default App;
